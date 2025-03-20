66bc767d44ff3f9604e2dc5afa8cbe4725cb7a4f
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-03-20 13:13:36 +0700

    membuat statefull widget

---
+++ listtodo/lib/page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class FormPage extends StatefulWidget {
+  const FormPage({super.key});
+
+  @override
+  State<FormPage> createState() => _FormPageState();
+}
