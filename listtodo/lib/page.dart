abeb5f619a82c5639e06325cc5cd18e6b9f93638
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-03-20 13:20:01 +0700

    button close untuk date picker

--- listtodo/lib/page.dart
+++ listtodo/lib/page.dart
@@ -45,3 +45,10 @@ class _FormPageState extends State<FormPage> {
                       decoration: TextDecoration.none
                     ),
                   ),
+                  GestureDetector(
+                    onTap: () => Navigator.of(context).pop(),
+                    child: const Icon(Icons.close, size: 25,)
+                  )
+                ],
+              ),
+            ),
