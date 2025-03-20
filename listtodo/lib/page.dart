730659fa882c6a6e8a88c39b9712d469fae75d4a
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-03-20 13:18:53 +0700

    merapikan tampilan

--- listtodo/lib/page.dart
+++ listtodo/lib/page.dart
@@ -28,3 +28,20 @@ class _FormPageState extends State<FormPage> {
             topRight: Radius.circular(30),
           ),
         ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Set Task Date & Time",
+                    style: TextStyle(
+                      fontFamily: "Montserrat",
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      decoration: TextDecoration.none
+                    ),
+                  ),
